497f5cc855c3d67c0b177e9e2b1a30b9e26f1798
Jacob <jacobm3@gmail.com>
2019-05-20 13:35:35 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@ output "website_bucket_id" {
   description = "This is actually the bucket name. Discussion here - https://github.com/hashicorp/terraform/issues/9845"
   value = "${aws_s3_bucket.website_bucket.id}"
 }
+
+output "bucket_domain_name" {
+  value = "${aws_s3_bucket.website_bucket.bucket_domain_name}"
+}
+
+output "bucket_arn" {
+  value = "${aws_s3_bucket.website_bucket.arn}"
+}
