952d1b51f5d3f190b6758de46f9090dcd3ff7215
Jacob <jacobm3@gmail.com>
2019-05-21 01:05:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,10 +30,10 @@ resource "aws_s3_bucket" "website_bucket" {
         }
     ]
 }
-  POLICY
+POLICY
   website {
     index_document = "index.html"
-    error_document = "index.html"
+    error_document = "err.html"
   }
 
   tags = {
